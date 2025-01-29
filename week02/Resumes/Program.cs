6e6a2a51a53e120060af13d87088ca8326b7b555
adamsoyama <oyama.adams@gmail.com>
2025-01-29 19:12:29 +0100

    Learning Activity - Abstraction: Added

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 2015;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Project Manager";
+        job2._company = "Microsoft";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._name = "Adams Oyama";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayDetails();
     }
 }
