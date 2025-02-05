eebd852e1764183a3eace79da2d97cac5b495721
adamsoyama <oyama.adams@gmail.com>
2025-02-05 17:01:49 +0100

    Week 05 Home work activity added

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,19 @@
+using System;
+public class Assignment
+    {
+        private string studentName;
+        private string topic;
+
+        // Constructor
+        public Assignment(string studentName, string topic)
+        {
+            this.studentName = studentName;
+            this.topic = topic;
+        }
+
+        // Method to get summary
+        public string GetSummary()
+        {
+            return $"{studentName} - {topic}";
+        }
+    }
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+
+       // Create a simple assignment
+            Assignment assignment = new Assignment("Kenneth Egumi", "Multiplication");
+            
+            // Call the GetSummary method and display the output
+            Console.WriteLine(assignment.GetSummary());
+
+             // Create a MathAssignment object and set its values
+            MathAssignment mathAssignment = new MathAssignment("Adams Oyama", "Fractions", "7.3", "8-19");
+
+            // Test GetSummary and GetHomeworkList methods
+            Console.WriteLine(mathAssignment.GetSummary());
+            Console.WriteLine(mathAssignment.GetHomeworkList());
+
+            {
+            // Create a WritingAssignment object and set its values
+            WritingAssignment writingAssignment = new WritingAssignment("Ekanachita Oyama", "European History", "The Causes of World War II");
+
+            // Test GetSummary and GetWritingInformation methods
+            Console.WriteLine(writingAssignment.GetSummary());
+            Console.WriteLine(writingAssignment.GetWritingInformation());
+        }
     }
 }
